f5400a58f1ff27bab1629daf23dfb5d364f83fee
Alankrit Arya <alankritarya15@gmail.com>
2021-07-01 21:21:17 +0530

    commit

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='user-login'),
+    path('users/register/', views.registerUser, name='use-registration'),
+    path('users/profile/',views.getUserProfile,name='user-profile'),
+    path('users/',views.getUsers,name='users'),
     path('',views.getRoutes,name='api-overview'),
     path('projects/',views.getProjects,name='projects-list'),
     path('projects/<str:pk>',views.getProject,name='specific-project'),
