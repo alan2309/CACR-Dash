158262a7a84da206592849ce4c62b5de068c73a1
Alankrit Arya <alankritarya15@gmail.com>
2021-07-21 19:56:19 +0530

    projects backend

--- frontend/cacr-dash/src/components/Programs.js
+++ frontend/cacr-dash/src/components/Programs.js
@@ -1,42 +1,58 @@
-import React from 'react'
-import Carousel from 'react-bootstrap/Carousel'
-import img1 from '../img/img1.jpeg'
-import img2 from '../img/img2.png'
-import img3 from '../img/img3.png'
-import '../CSS/Programs.css'
-import ProjectCard from './ProjectCard'
+import React, { useState, useEffect } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import img1 from "../img/img1.jpeg";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import "../CSS/Programs.css";
+import ProjectCard from "./ProjectCard";
+import axios from "axios";
 
 function Programs() {
-    return (
-        <div className="car container-flex">
-          <Carousel controls={false} fade>
-  <Carousel.Item interval={3000}>
-    <img
-      className="d-inline-block w-100 h-25"
-      src={img1}
-      alt="First slide"
-      height="20px"
-    />
-  </Carousel.Item>
-  <Carousel.Item interval={3000}>
-    <img
-      className="d-inline-block w-100 h-25"
-      src={img2}
-      alt="Second slide"
-    />
-  </Carousel.Item>
-  <Carousel.Item interval={4000}>
-    <img
-      className="d-inline-block w-100 h-25"
-      src={img3}
-      alt="Third slide"
-    />
-  </Carousel.Item>
-</Carousel>
-<h4 align="center">Our Programs</h4>
-    <ProjectCard/>
-        </div>
-    )
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/projects").then((res) => {
+      setProjects(res.data);
+    });
+  }, []);
+  return (
+    <div className="car container-flex">
+      <Carousel controls={false} fade>
+        <Carousel.Item interval={3000}>
+          <img
+            className="d-inline-block w-100 h-25"
+            src={img1}
+            alt="First slide"
+            height="20px"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={3000}>
+          <img
+            className="d-inline-block w-100 h-25"
+            src={img2}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={4000}>
+          <img
+            className="d-inline-block w-100 h-25"
+            src={img3}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+      <h4 align="center">Our Programs</h4>
+      {projects.map((proj) => {
+        return (
+          <ProjectCard
+            img={proj.image}
+            title={proj.title}
+            description={proj.description}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
-export default Programs
+export default Programs;
--- frontend/cacr-dash/src/components/ProjectCard.js
+++ frontend/cacr-dash/src/components/ProjectCard.js
@@ -1,35 +1,39 @@
-import React from 'react'
-import img1 from '../img/img1.jpeg'
-import '../CSS/Programs.css'
+import React from "react";
+import img1 from "../img/img1.jpeg";
+import "../CSS/Programs.css";
 
-const firstProgram ={
-    img:img1,
-    title:'Wash Initiative',
-    description:'Awareness sessions and distribution of personal hygiene kits to children.'
-}
+const firstProgram = {
+  img: img1,
+  title: "Wash Initiative",
+  description:
+    "Awareness sessions and distribution of personal hygiene kits to children."
+};
 
-function ProjectCard() {
-    return (
+function ProjectCard(props) {
+  const { img, title, description } = props;
+  return (
     <div class=" pro container-fluid">
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-            <Program img={firstProgram.img} title={firstProgram.title} description={firstProgram.description}/>
-        </div>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <Program img={img} title={title} description={description} />
+      </div>
     </div>
-    )
+  );
 }
 const Program = (props) => {
-    const {img, title, description} = props;
-    return <div>
-    <div className="col">
+  const { img, title, description } = props;
+  return (
+    <div>
+      <div className="col">
         <div className="card h-100 ">
-            <img src={img} class="card-img-top" alt="program image"/>
-                <div className="card-body">
-                    <h4 className="card-title">{title}</h4>
-                    <p className="card-text">{description}</p>
-                </div>
-            </div>
+          <img src={img} class="card-img-top" alt={title} />
+          <div className="card-body">
+            <h4 className="card-title">{title}</h4>
+            <p className="card-text">{description}</p>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-  }
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
