44e798bfa11ffeaec74e2ae2ce796bb9c0ceeafc
Alankrit Arya <alankritarya15@gmail.com>
2021-07-28 13:03:39 +0530

    Update ProjectCard.js

--- frontend/cacr-dash/src/components/ProjectCard.js
+++ frontend/cacr-dash/src/components/ProjectCard.js
@@ -1,25 +1,26 @@
 import React from "react";
 import "../CSS/Programs.css";
+import { Link } from "react-router-dom";
 
 function ProjectCard(props) {
   const { img, title, description } = props;
-  return (
-        <Program img={img} title={title} description={description} />
-  );
+  return <Program img={img} title={title} description={description} />;
 }
 const Program = (props) => {
-  const { img, title, description } = props;
+  const { img, title, description, id } = props;
   return (
     <div>
-      <div className="col">
-        <div className="card h-100 ">
-          <img src={img} class="card-img-top" alt={title} />
-          <div className="card-body">
-            <h4 className="card-title">{title}</h4>
-            <p className="card-text">{description}</p>
+      <Link to={`/programs/status/${id}`}>
+        <div className="col">
+          <div className="card h-100 ">
+            <img src={img} class="card-img-top" alt={title} />
+            <div className="card-body">
+              <h4 className="card-title">{title}</h4>
+              <p className="card-text">{description}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
