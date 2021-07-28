eafdffec129715137b23e51abd9abb29935e128b
Alankrit Arya <alankritarya15@gmail.com>
2021-07-28 13:05:03 +0530

    backend connected
    status connected to backend

--- frontend/cacr-dash/src/components/Statuses.js
+++ frontend/cacr-dash/src/components/Statuses.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import '../CSS/ProgressBar.css'
-import Status from './Status'
+import React, { useState, useEffect } from "react";
+import "../CSS/ProgressBar.css";
+import Status from "./Status";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 
 function Statuses() {
-    return (
-        <div className="container">
-            <h4>Program Completiton Status</h4>
-            <hr/>
-            <Status task ="Distribute Sanitary Pads" target="90" completed="60"/>
-        </div>
-    );
+  const [tasks, setTasks] = useState([]);
+  var id = useParams().id;
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/projects/${id}/task`).then((res) => {
+      setTasks(res.data);
+    });
+  }, []);
+  return (
+    <div className="container">
+      <h4>Program Completiton Status</h4>
+      <hr />
+      {tasks.map((task) => {
+        return (
+          <Status
+            task={task.name}
+            target={task.target}
+            completed={task.completed}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
-export default Statuses
+export default Statuses;
