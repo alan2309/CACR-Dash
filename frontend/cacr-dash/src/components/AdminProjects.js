e03535c17d68aae7fd020a1b99432ea0ae12e08f
Alankrit Arya <alankritarya15@gmail.com>
2021-08-24 01:18:05 +0530

    fa icons

--- frontend/cacr-dash/src/components/AdminProjects.js
+++ frontend/cacr-dash/src/components/AdminProjects.js
@@ -1,12 +1,25 @@
-const AdminProjects = ({ projects }) => {
-    return (
-      <>
-        {projects.map((proj) => {
-          return <h3>{proj.title}</h3>;
-        })}
-      </>
-    );
-  };
-  
-  export default AdminProjects;
-  
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+const AdminProjects = ({ projects}) => {
+  return (
+    <>
+      {projects.map((proj) => {
+        return (
+          <>
+            <p key={proj._id}>
+              {proj.title}
+              <Link to={`/admin/programs/${proj._id}/edit`}>
+                <FontAwesomeIcon icon={faEdit} />
+              </Link>
+              <FontAwesomeIcon icon={faTrash} value={proj._id} />
+            </p>
+          </>
+        );
+      })}
+    </>
+  );
+};
+
+export default AdminProjects;
