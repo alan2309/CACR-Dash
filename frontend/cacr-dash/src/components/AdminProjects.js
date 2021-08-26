418f043c128b010aaf5bec7bb5981fb416553291
Alankrit Arya <alankritarya15@gmail.com>
2021-08-26 17:45:45 +0530

    admin table

--- frontend/cacr-dash/src/components/AdminProjects.js
+++ frontend/cacr-dash/src/components/AdminProjects.js
@@ -1,32 +1,41 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
-import "../CSS/AdminProjects.css"
+import { Button, Table } from "react-bootstrap";
+import "../CSS/AdminProjects.css";
 
 const AdminProjects = ({ projects, deleteHandler }) => {
   return (
     <>
-      {projects.map((proj) => {
-        return (
-          <>
-            <p key={proj._id}>
-              <p className="title">{proj.title}</p>
-              <Link to={`/admin/programs/${proj._id}/edit`}>
-              <button
-                className="btn btn-sm btn-danger updel">Update this program
-                <FontAwesomeIcon icon={faEdit} /></button>
-              </Link>
-              <button
-                className="btn btn-sm btn-danger updel"
-                onClick={() => deleteHandler(proj._id)}
-              >Delete this program
-                <FontAwesomeIcon icon={faTrash} value={proj._id} />
-              </button>
-            </p>
-          </>
-        );
-      })}
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Project Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((proj, index) => {
+            return (
+              <tr key={proj._id}>
+                <td>{index + 1}</td>
+                <td>{proj.title}</td>
+                <td>
+                  <Link to={`/admin/programs/${proj._id}/edit`}>
+                    <FontAwesomeIcon style={{ color: "blue" }} icon={faEdit} />
+                  </Link>
+                </td>
+                <button
+                  className="btn btn-sm btn-danger updel"
+                  onClick={() => deleteHandler(proj._id)}
+                >
+                  <FontAwesomeIcon icon={faTrash} value={proj._id} />
+                </button>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
     </>
   );
 };
