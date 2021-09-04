5d8962a022c6c698d8bcc7ea7ea2f59e8982defb
Alankrit Arya <alankritarya15@gmail.com>
2021-09-04 07:41:10 +0530

    Update PrivateScreen.js

--- frontend/cacr-dash/src/components/PrivateScreen.js
+++ frontend/cacr-dash/src/components/PrivateScreen.js
@@ -1,10 +1,5 @@
-const PrivateScreen = ()=>{
-    return <h1>Private</h1>
-}
 const PrivateScreen = () => {
-    return <h1>Private</h1>;
+    return (<h1>Private</h1>);
   };
-  
-  export default PrivateScreen;
   
 export default PrivateScreen;
